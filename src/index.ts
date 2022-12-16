232338cf50d41cdae54f3b503f71e2bc5191bc78
tomekskuta <tomasz.skuta@monterail.com>
2022-12-16 23:22:53 +0100

    [chore] Setup project

---
+++ src/index.ts
@@ -0,0 +1,16 @@
+import { App } from '@slack/bolt';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+const app = new App({
+  token: process.env.SLACK_BOT_TOKEN,
+  signingSecret: process.env.SLACK_SIGNING_SECRET,
+});
+
+(async () => {
+  // Start your app
+  await app.start(Number(process.env.PORT) ?? 3000);
+
+  console.log('⚡️ Bolt app is running!');
+})();
