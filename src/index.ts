9e331503b217f6afa0e4f9c06fc8cceedd6fb5cf
tomekskuta <tomasz.skuta@monterail.com>
2022-12-19 16:48:05 +0100

    [fix] Revert socketMode, add custom route from cron

--- src/index.ts
+++ src/index.ts
@@ -9,7 +9,18 @@ const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
   appToken: process.env.SLACK_APP_TOKEN,
+  socketMode: true,
   port: Number(process.env.PORT) || 3000,
+  customRoutes: [
+    {
+      path: '/wake-up',
+      method: ['GET'],
+      handler: (req, res) => {
+        res.writeHead(200);
+        res.end('OK, Im awake');
+      },
+    },
+  ],
 });
 
 app.event('app_mention', async (eventMiddlewareArrgs) => {
