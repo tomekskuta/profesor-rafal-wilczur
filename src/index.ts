ff5f63acf36677c22a27eb249f487989f5deb7df
tomekskuta <tomasz.skuta@monterail.com>
2022-12-19 16:41:07 +0100

    [fix] Remove socket mode

--- src/index.ts
+++ src/index.ts
@@ -8,7 +8,6 @@ dotenv.config();
 const app = new App({
   token: process.env.SLACK_BOT_TOKEN,
   signingSecret: process.env.SLACK_SIGNING_SECRET,
-  socketMode: true,
   appToken: process.env.SLACK_APP_TOKEN,
   port: Number(process.env.PORT) || 3000,
 });
