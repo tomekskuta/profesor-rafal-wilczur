6880189401969a3ffbc08af216e632dbd768a2e2
tomekskuta <tomasz.skuta@monterail.com>
2022-12-21 08:52:58 +0100

    [feat] Add nowomowa message feature

--- src/messageFeatures/index.ts
+++ src/messageFeatures/index.ts
@@ -2,11 +2,13 @@ import { App } from '@slack/bolt';
 
 import { mention } from './mention';
 import { goodBye } from './goodBye';
+import { nowomowa } from './nowomowa';
 
 const useMessageFeatures = (app: App): void => {
   // TODO: Here you may add and run your message feature
   mention(app);
   goodBye(app);
+  nowomowa(app);
 };
 
 export default useMessageFeatures;
