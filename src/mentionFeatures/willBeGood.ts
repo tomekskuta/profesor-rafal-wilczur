5136486dc8679c718b7322d61a16153335ee6f70
tomekskuta <tomasz.skuta@monterail.com>
2023-06-16 09:33:25 +0200

    [fix] Change way of showing video in willBeGood feature

--- src/mentionFeatures/willBeGood.ts
+++ src/mentionFeatures/willBeGood.ts
@@ -8,14 +8,25 @@ const question: MentionFeature['question'] = [
   'bedzie dobrze',
 ];
 
-const answers = [
-  'będzie dobrze!',
-  'nie będzie.',
-  'https://www.youtube.com/watch?v=7ZjbOzsIaRs',
-];
+const answers = ['będzie dobrze!', 'nie będzie.', 'VIDEO_LINK'];
 
 const middleware: MentionFeature['middleware'] = async ({ say, event }) => {
   const { user } = event;
+
+  const answer = getRandomElement(answers);
+
+  if (answer === 'VIDEO_LINK') {
+    await say({
+      blocks: [
+        {
+          type: 'video',
+          video_url: 'https://www.youtube.com/embed/7ZjbOzsIaRs',
+        },
+      ],
+    });
+
+    return;
+  }
 
   await say(`<@${user}> ${getRandomElement(answers)}`);
 };
