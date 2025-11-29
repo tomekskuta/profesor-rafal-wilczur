2369fe3bd1554128a46e1c8ffa41f7b2e5663b1d
Tomek Skuta <kontakt@tomekskuta.pl>
2025-11-29 15:27:29 +0100

    feat(members): restore memberLefts event handling in Slack integration

--- src/membersEvents/index.ts
+++ src/membersEvents/index.ts
@@ -1,10 +1,9 @@
 import { App } from '@slack/bolt';
 
 import { memberJoins } from './memberJoins';
-// TODO: Uncomment when will be safe time :(
-// import { memberLefts } from './memberLefts';
+import { memberLefts } from './memberLefts';
 
 export default (app: App): void => {
   memberJoins(app);
-  // memberLefts(app);
+  memberLefts(app);
 };
